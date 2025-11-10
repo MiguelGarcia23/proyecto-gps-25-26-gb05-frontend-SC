7c28721b3f2f378b37f32eae92e905eff309650f
Danny5249 <dbarrantp@alumnos.unex.es>
2025-11-10 18:04:41 +0100

    feat:GB05-48 Desarrollar la vista para albumes

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,18 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router';
 import { AuthProvider } from './contexts/auth.context.tsx';
 import { ToastProvider } from './contexts/toast.context.tsx';
+import { ArtistProvider } from './contexts/artists.context.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<BrowserRouter>
 			<ToastProvider>
 				<AuthProvider>
-					<div className="min-h-screen bg-base-300">
-						<App />
-					</div>
+					<ArtistProvider>
+						<div className="min-h-screen bg-base-300">
+							<App />
+						</div>
+					</ArtistProvider>
 				</AuthProvider>
 			</ToastProvider>
 		</BrowserRouter>
