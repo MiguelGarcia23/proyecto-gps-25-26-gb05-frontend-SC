b3e92b0dfdcbc00594aec4aa8edd1f4fd724ae49
Danny5249 <dbarrantp@alumnos.unex.es>
2025-11-12 15:44:23 +0100

    feat: GB05-67 Desarrollar la vista de crear producto de merchandising

--- src/contexts/artists.context.tsx
+++ src/contexts/artists.context.tsx
@@ -19,7 +19,12 @@ interface ArtistContextType{
 		albumSong: string[],
 		albumCover: File,
 	) => Promise<void>;
-
+	uploadMerch: (
+		productName: string,
+		productMerch: string,
+		productType: string[],
+		productPrice: string,
+	) => Promise<void>;
 }
 
 
@@ -68,8 +73,29 @@ export const ArtistProvider = ({ children }: { children: ReactNode }) => {
 		})
 	};
 
+		const uploadMerch = async (
+			productName: string,
+			productMerch: string,
+			productType: string[],
+			productPrice: string,
+		) => {
+			const formData = new FormData();
+			formData.append('productName', productName);
+			formData.append('productMerch', productMerch);
+			formData.append('productType', productType);
+			formData.append('productPrice', productPrice);
+
+			const response = await fetch(`${window.location.origin}/api/v1/songs`,{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'multipart/form-data',
+					Authorization: `Bearer ${auth.session?.access_token}`
+				},
+				body: formData
+			})
+		}
 	return(
-		<ArtistContext.Provider value={{uploadSong, uploadAlbum}}>
+		<ArtistContext.Provider value={{uploadSong, uploadAlbum, uploadMerch}}>
 			{children}
 		</ArtistContext.Provider>
 	)
